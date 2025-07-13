d1a1d4622ac117d27d970201b622d59a580601d4
Piyush Sankhala <piyushsankhala2004@gmail.com>
2025-07-13 18:22:43 +0530

    ww

--- src/pages/chatroom.jsx
+++ src/pages/chatroom.jsx
@@ -78,6 +78,7 @@ export default function Chatroom() {
 
   useEffect(() => {
     accessChat();
+    fetchCurrentUser();
     const interval = setInterval(() => accessChat(), 1000);
     return () => clearInterval(interval);
   }, []);
