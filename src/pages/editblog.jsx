aaefaf340f091015ca20cb0e0dde943e8285c332
Piyush Sankhala <piyushsankhala2004@gmail.com>
2025-07-15 17:50:10 +0530

    kk

--- src/pages/editblog.jsx
+++ src/pages/editblog.jsx
@@ -48,7 +48,7 @@ export default function EditBlog() {
             <input
               type="file"
               accept="image/*"
-              value={image}
+              
               onChange={(e) => setImage(e.target.files[0])}
               className="w-full border border-gray-300 rounded px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Enter image URL or leave blank"
